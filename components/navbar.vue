f34238fcd8348a3b8136011a2aeb9950a678a01e
adarsh <sanakihacker@gmail.com>
2023-07-11 11:11:58 +0530

    fix title

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,5 +1,9 @@
 <template>
     <v-card class="pt-4" flat>
-        <v-card-title class="text-center text-h3">Resume Builder </v-card-title>
+        <v-card-title
+            class="text-h4 text-md-h3 text-lg-h2 font-weight-bold text-center"
+            >Resume Builder
+        </v-card-title>
+        <v-divider thickness="8"></v-divider>
     </v-card>
 </template>
